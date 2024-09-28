0b22376c9b6f34df0ee7fa74785dd22d1d0064f2
wujiaan2022 <emmawu2004@gmail.com>
2024-09-28 23:07:45 +0200

    create the basic user interface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import products
-from store import Store
+import store
+
 
 # bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
 # mac = Product("MacBook Air M2", price=1450, quantity=100)
@@ -15,12 +16,39 @@ from store import Store
 # bose.show()
 
 
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
+# product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+#                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+#                 products.Product("Google Pixel 7", price=500, quantity=250),
+#                ]
+#
+# store = Store(product_list)
+# products = store.get_all_products()
+# print(store.get_total_quantity())
+# print(store.order([(products[0], 1), (products[1], 2)]))
+
+
+# setup initial stock of inventory
+product_list = [ products.Product("MacBook Air M2", price=1450, quantity=100),
+                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 products.Product("Google Pixel 7", price=500, quantity=250)
                ]
 
-store = Store(product_list)
-products = store.get_all_products()
-print(store.get_total_quantity())
-print(store.order([(products[0], 1), (products[1], 2)]))
+best_buy = store.Store(product_list)
+
+
+def start(store_object):
+
+    menu = {
+        "1": "List all products in store",
+        "2": "Show total amount in store",
+        "3": "Make an order",
+        "4": "Quit"
+    }
+
+    title = "Store Menu"
+    dash_line = "-" * len(title)
+    print(title)
+    print(dash_line)
+
+    for key, value in menu.items():
+        print(f"{key}. {value}")
